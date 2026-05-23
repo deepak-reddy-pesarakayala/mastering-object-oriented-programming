400100fb5812511fcc09117a7ed07af1713821a7
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-05-24 00:41:26 +0530

    ✅ Add solution for: Counting Even and Odd Numbers

---
+++ exercises/CountingEvenAndOddNumbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int evenCount = 0;
+        int oddCount = 0;
+        for (int i = 0; i < n; i++) {
+            int num = sc.nextInt();
+            if (num % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+        }
+        System.out.println("Even Numbers Count: " + evenCount);
+        System.out.println("Odd Numbers Count: " + oddCount);
+    }
+}
